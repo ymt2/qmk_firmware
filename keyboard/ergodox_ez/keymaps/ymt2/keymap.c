4e3eb4bc2da04b2e0f0329fec7a9623418d7d493
Tatsuya Yamamoto <tatsuya@ymt2.net>
2016-05-09 00:54:07 +0900

    tweak for JIS_EISUU/JIS_KANA

--- keyboard/ergodox_ez/keymaps/ymt2/keymap.c
+++ keyboard/ergodox_ez/keymaps/ymt2/keymap.c
@@ -14,7 +14,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |  RGui  |
- * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------| Eisuu|           | Kana |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |AltShf|  Grv |  '"  | Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
@@ -34,7 +34,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,         KC_1,         KC_2,     KC_3,     KC_4,   KC_5,   KC_LEFT,
         KC_TAB,         KC_Q,         KC_W,     KC_E,     KC_R,   KC_T,   TG(SYMB),
         KC_LCTRL,       KC_A,         KC_S,     KC_D,     KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,     KC_C,     KC_V,   KC_B,   ALL_T(KC_NO),
+        KC_LSFT,        KC_Z,         KC_X,     KC_C,     KC_V,   KC_B,   KC_POWER,
         LALT(KC_LSFT),  KC_GRV,       KC_QUOT,  KC_LEFT,  KC_RGHT,
                                                 ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_RGHT,       KC_6,   KC_7,   KC_8,    KC_9,     KC_0,      KC_MINS,
         TG(SYMB),      KC_Y,   KC_U,   KC_I,    KC_O,     KC_P,      KC_BSLS,
                        KC_H,   KC_J,   KC_K,    KC_L,     KC_SCLN,   KC_RGUI,
-        MEH_T(KC_NO),  KC_N,   KC_M,   KC_COMM, KC_DOT,   KC_SLSH,   KC_RSFT,
+        KC_F13,        KC_N,   KC_M,   KC_COMM, KC_DOT,   KC_SLSH,   KC_RSFT,
                                KC_UP,  KC_DOWN, KC_LBRC,  KC_RBRC,   KC_FN1,
         KC_LALT,       KC_ESC,
         KC_PGUP,
