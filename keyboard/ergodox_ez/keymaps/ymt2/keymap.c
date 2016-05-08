491977cd7998911628f88c268d69630f2e5edad4
Tatsuya Yamamoto <tatsuya@ymt2.net>
2016-05-08 16:52:36 +0900

    modify layer #0 right hand

--- keyboard/ergodox_ez/keymaps/ymt2/keymap.c
+++ keyboard/ergodox_ez/keymaps/ymt2/keymap.c
@@ -14,17 +14,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L2|  LGui  |
+ * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |  LGui  |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   |AltShf|  Grv |  '"  | Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
  *   `----------------------------------'"                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        | App  | LGui |       | Alt  |Ctrl/Esc|
+ *                                        | App  | LGui |       | Alt  |  Esc |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | LCtrl|Backsp|------|       |------|  Tab   |Enter |
+ *                                 | LCtrl|Backsp|------|       |------|  Space |Enter |
  *                                 |      |ace   | End  |       | PgDn |        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_EQL,         KC_1,         KC_2,     KC_3,     KC_4,   KC_5,   KC_LEFT,
-        KC_TAB,         KC_Q,         KC_W,     KC_E,     KC_R,   KC_T,   TG(1),
+        KC_TAB,         KC_Q,         KC_W,     KC_E,     KC_R,   KC_T,   TG(SYMB),
         KC_LCTRL,       KC_A,         KC_S,     KC_D,     KC_F,   KC_G,
         KC_LSFT,        KC_Z,         KC_X,     KC_C,     KC_V,   KC_B,   ALL_T(KC_NO),
         LALT(KC_LSFT),  KC_GRV,       KC_QUOT,  KC_LEFT,  KC_RGHT,
@@ -41,14 +41,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                               KC_HOME,
                                                 KC_LCTRL,KC_BSPC,KC_END,
         // right hand
-             KC_RGHT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
-             TG(1),       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_BSLS,
-                          KC_H,   KC_J,   KC_K,   KC_L,   LT(MDIA, KC_SCLN),KC_LGUI,
-             MEH_T(KC_NO),KC_N,   KC_M,   KC_COMM,KC_DOT, CTL_T(KC_SLSH),   KC_RSFT,
-                                  KC_UP,  KC_DOWN,KC_LBRC,KC_RBRC,          KC_FN1,
-             KC_LALT,        CTL_T(KC_ESC),
-             KC_PGUP,
-             KC_PGDN,KC_TAB, KC_ENT
+        KC_RGHT,       KC_6,   KC_7,   KC_8,    KC_9,     KC_0,      KC_MINS,
+        TG(SYMB),      KC_Y,   KC_U,   KC_I,    KC_O,     KC_P,      KC_BSLS,
+                       KC_H,   KC_J,   KC_K,    KC_L,     KC_SCLN,   KC_LGUI,
+        MEH_T(KC_NO),  KC_N,   KC_M,   KC_COMM, KC_DOT,   KC_SLSH,   KC_RSFT,
+                               KC_UP,  KC_DOWN, KC_LBRC,  KC_RBRC,   KC_FN1,
+        KC_LALT,       KC_ESC,
+        KC_PGUP,
+        KC_PGDN,       KC_SPC, KC_ENT
     ),
 /* Keymap 1: Symbol Layer
  *
