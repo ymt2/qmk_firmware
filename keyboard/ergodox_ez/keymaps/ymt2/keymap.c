d26ffaa583a70fda8b4a429e0260cf9bd9764c59
Tatsuya Yamamoto <tatsuya@ymt2.net>
2016-05-09 00:06:22 +0900

    fix

--- keyboard/ergodox_ez/keymaps/ymt2/keymap.c
+++ keyboard/ergodox_ez/keymaps/ymt2/keymap.c
@@ -13,7 +13,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
  * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |  LGui  |
+ * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |  RGui  |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // right hand
         KC_RGHT,       KC_6,   KC_7,   KC_8,    KC_9,     KC_0,      KC_MINS,
         TG(SYMB),      KC_Y,   KC_U,   KC_I,    KC_O,     KC_P,      KC_BSLS,
-                       KC_H,   KC_J,   KC_K,    KC_L,     KC_SCLN,   KC_LGUI,
+                       KC_H,   KC_J,   KC_K,    KC_L,     KC_SCLN,   KC_RGUI,
         MEH_T(KC_NO),  KC_N,   KC_M,   KC_COMM, KC_DOT,   KC_SLSH,   KC_RSFT,
                                KC_UP,  KC_DOWN, KC_LBRC,  KC_RBRC,   KC_FN1,
         KC_LALT,       KC_ESC,
