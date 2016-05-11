66cedd1bb18851519768bdbf26c20264226c2871
ymt2 <tatsuya@ymt2.net>
2016-05-12 02:49:34 +0900

    tweak modifiers

--- keyboard/ergodox_ez/keymaps/ymt2/keymap.c
+++ keyboard/ergodox_ez/keymaps/ymt2/keymap.c
@@ -11,13 +11,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |   =    |   1  |   2  |   3  |   4  |   5  | Left |           | Right|   6  |   7  |   8  |   9  |   0  |   -    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab    |   Q  |   W  |   E  |   R  |   T  |  L1  |           |  L1  |   Y  |   U  |   I  |   O  |   P  |   \    |
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  | Del  |           |  ;   |   Y  |   U  |   I  |   O  |   P  |   \    |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |  RGui  |
  * |--------+------+------+------+------+------| Hyper|           | Meh  |------+------+------+------+------+--------|
- * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  | RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |/eisuu|           | /kana|   N  |   M  |   ,  |   .  |   /  | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |AltShf|  Grv |  '"  | Left | Right|                                       |  Up  | Down |   [  |   ]  | ~L1  |
+ *   |AltShf|  Grv |  '"  | LAlt | LGui |                                       | RGui | RAlt |   [  |   ]  | ~L1  |
  *   `----------------------------------'"                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        | App  | LGui |       | Alt  |  Esc |
@@ -32,19 +32,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
         KC_EQL,         KC_1,         KC_2,     KC_3,     KC_4,   KC_5,   KC_LEFT,
-        KC_TAB,         KC_Q,         KC_W,     KC_E,     KC_R,   KC_T,   TG(SYMB),
+        KC_TAB,         KC_Q,         KC_W,     KC_E,     KC_R,   KC_T,   KC_DELT,
         KC_LCTRL,       KC_A,         KC_S,     KC_D,     KC_F,   KC_G,
-        KC_LSFT,        KC_Z,         KC_X,     KC_C,     KC_V,   KC_B,   ALL_T(KC_NO),
-        LALT(KC_LSFT),  KC_GRV,       KC_QUOT,  KC_LEFT,  KC_RGHT,
+        KC_LSFT,        KC_Z,         KC_X,     KC_C,     KC_V,   KC_B,   ALL_T(KC_LANG2),
+        LALT(KC_LSFT),  KC_GRV,       KC_QUOT,  KC_LALT,  KC_LGUI,
                                                 ALT_T(KC_APP),  KC_LGUI,
                                                               KC_HOME,
                                                 KC_LCTRL,KC_BSPC,KC_END,
         // right hand
         KC_RGHT,       KC_6,   KC_7,   KC_8,    KC_9,     KC_0,      KC_MINS,
-        TG(SYMB),      KC_Y,   KC_U,   KC_I,    KC_O,     KC_P,      KC_BSLS,
-                       KC_H,   KC_J,   KC_K,    KC_L,     KC_SCLN,   KC_RGUI,
-        MEH_T(KC_NO),  KC_N,   KC_M,   KC_COMM, KC_DOT,   KC_SLSH,   KC_RSFT,
-                               KC_UP,  KC_DOWN, KC_LBRC,  KC_RBRC,   KC_FN1,
+        KC_SCLN,       KC_Y,   KC_U,   KC_I,    KC_O,     KC_P,      KC_BSLS,
+                       KC_H,   KC_J,   KC_K,    KC_L,     MT(KC_SCLN, KC_FN2),   KC_RGUI,
+        MEH_T(KC_LANG1),  KC_N,   KC_M,   KC_COMM, KC_DOT,   KC_SLSH,   KC_RSFT,
+                               KC_RGUI,  KC_RALT, KC_LBRC,  KC_RBRC,   KC_FN1,
         KC_LALT,       KC_ESC,
         KC_PGUP,
         KC_PGDN,       KC_ENT, KC_SPC
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * |        |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |   F12  |
+ * |        |      |      |      | Reset|      |      |           |      |      |      |      |      |      |   F12  |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |        | VolUp| VolDn| Mute | Eject|      |------|           |------|      |      |      |  UP  |      |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [SYMB] = KEYMAP(
        // left hand
        KC_TRNS,KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,RESET,  KC_TRNS,KC_TRNS,
        KC_TRNS,KC_VOLU,KC_VOLD,KC_MUTE,KC_EJCT,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
@@ -94,7 +94,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 const uint16_t PROGMEM fn_actions[] = {
-    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB)                // FN1 - Momentary Layer 1 (Symbols)
+    [1] = ACTION_LAYER_TAP_TOGGLE(SYMB),                 // FN1 - Momentary Layer 1 (Symbols)
+    [2] = ACTION_MODS_ONESHOT(MOD_LSFT),                 // FN2 - Sticky shift
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt)
